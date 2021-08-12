5fbe7e79b430f3d5230c4c0b6a70e1963b380caf
Alex <alex.fesak@reinsight.se>
2021-08-12 12:06:28 +0200

    adjusted load script in case local user rules already exist

--- BestPractices/LoadBPARules.cs
+++ BestPractices/LoadBPARules.cs
@@ -9,6 +9,7 @@ if (System.IO.File.Exists(downloadLoc))
 {
     if (System.Windows.Forms.MessageBox.Show("Would you like to overwrite the existing BPARules.json file?","Overwrite Existing BPA Rules",System.Windows.Forms.MessageBoxButtons.YesNo,System.Windows.Forms.MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
     {
+        w.DownloadFile(url, downloadLoc);
         System.Windows.Forms.MessageBox.Show(dlMessage,"Overwrite Existing BPA Rules",System.Windows.Forms.MessageBoxButtons.OK,System.Windows.Forms.MessageBoxIcon.Information);
     }
     else
@@ -18,6 +19,6 @@ if (System.IO.File.Exists(downloadLoc))
 }
 else
 {
-    System.Windows.Forms.MessageBox.Show(dlMessage,"Download BPA Rules",System.Windows.Forms.MessageBoxButtons.OK,System.Windows.Forms.MessageBoxIcon.Information);
     w.DownloadFile(url, downloadLoc);
+    System.Windows.Forms.MessageBox.Show(dlMessage,"Download BPA Rules",System.Windows.Forms.MessageBoxButtons.OK,System.Windows.Forms.MessageBoxIcon.Information);
 }
