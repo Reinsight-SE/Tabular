dac6a387866d8831af93fe9677ce3f61e4b82419
Alex <alex.fesak@reinsight.se>
2021-08-24 19:28:48 +0200

    extended export of BPA validation details with Ignored attribute

--- BestPractices/ExportBPAResults.cs
+++ BestPractices/ExportBPAResults.cs
@@ -6,11 +6,11 @@ bpa.SetModel(Model);
 var sb = new System.Text.StringBuilder();
 string newline = Environment.NewLine;
 
-sb.Append("RuleCategory" + '\t' + "RuleName" + '\t' + "ObjectName" + '\t' + "ObjectType" + '\t' + "RuleSeverity" + '\t' + "HasFixExpression" + newline);
+sb.Append("RuleCategory" + '\t' + "RuleName" + '\t' + "ObjectName" + '\t' + "ObjectType" + '\t' + "Ignored" + '\t' + "RuleSeverity" + '\t' + "HasFixExpression" + newline);
 
 foreach (var a in bpa.AnalyzeAll().ToList())
 {
-    sb.Append(a.Rule.Category + '\t' + a.RuleName + '\t' + a.ObjectName + '\t' + a.ObjectType + '\t' + a.Rule.Severity + '\t' + a.CanFix + newline);
+    sb.Append(a.Rule.Category + '\t' + a.RuleName + '\t' + a.ObjectName + '\t' + a.ObjectType + '\t' + a.Ignored + '\t' + a.Rule.Severity + '\t' + a.CanFix + newline);
 }
 
 sb.Output();
